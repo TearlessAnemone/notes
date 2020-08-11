fc7b78f1495849ba5a06445a27b591d5151ddc6d
TearlessAnemone <40964662+TearlessAnemone@users.noreply.github.com>
2020-08-11 17:04:37 -0400

    Create RockPaperScissors.js

---
+++ RockPaperScissors.js
@@ -0,0 +1,11 @@
+// declare a function called add that accepts two arguments `x` and `y`
+function add( x, y ) {
+  // add arguments together and return to invoke
+  return x + y;
+}
+
+// declare a and set it to the result (return value) of `add`
+const a = add( 1, 2 );
+console.log( a );
+add( 3, 4 );
+add( 4, 5 );
